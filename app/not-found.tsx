ac0af104d01ef180e5efbf6e5ea85c1c81c7cc2f
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-23 17:31:16 +0300

    Integrate Sentry for error tracking and monitoring
    - Added Sentry configuration to the Next.js application by wrapping the existing NextConfig with `withSentryConfig`.
    - Updated `.gitignore` to include the Sentry configuration file `.env.sentry-build-plugin`.
    - Added `@sentry/nextjs` as a new dependency in `package.json` and `package-lock.json` to enable Sentry's functionality.

---
+++ app/not-found.tsx
@@ -0,0 +1,31 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+export const metadata = {
+  title: "Page Not Found",
+};
+
+export default function NotFound() {
+  return (
+    <div className="px-2 w-full">
+      <div className="mx-auto py-4 flex flex-col justify-center items-center gap-4 min-h-screen">
+        <h2 className="text-2xl">Page Not Found</h2>
+        <Image
+          className="m-0 rounded-xl"
+          src="/images/not-found-1024x1024.png"
+          width={300}
+          height={300}
+          sizes="300px"
+          alt="Page Not Found"
+          priority={true}
+          title="Page Not Found"
+        />
+        <Link href="/" className={cn(buttonVariants({ variant: "default" }))}>
+          Go to Home
+        </Link>
+      </div>
+    </div>
+  );
+}
