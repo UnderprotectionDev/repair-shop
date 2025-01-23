c4d0a0e975cfe27c22bad1191517796299a1e3f1
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-01-23 15:35:50 +0300

    Update dependencies and integrate theme provider
    - Added new dependencies: @radix-ui/react-dropdown-menu and next-themes for enhanced UI components and theme management.
    - Wrapped the application in a ThemeProvider in layout.tsx to support theme switching.
    - Introduced ModeToggle component in the header for user-controlled theme switching.

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import { HomeIcon, File, UsersRound } from "lucide-react";
 import Link from "next/link";
 
 import NavButton from "@/components/nav-button";
+import { ModeToggle } from "./mode-toggle";
 
 export default function Header() {
   return (
@@ -25,6 +26,8 @@ export default function Header() {
           <NavButton href="/tickets" label="Tickets" icon={File} />
 
           <NavButton href="/customers" label="Customers" icon={UsersRound} />
+
+          <ModeToggle />
         </div>
       </div>
     </header>
